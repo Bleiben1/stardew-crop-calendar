f4692ad390b505df8b8ed6bea71939007473f4f2
rodrigo <rodri47hn@gmail.com>
2021-01-15 05:57:02 -0300

    added updateUserGrow to store changes to one crop planted

--- app-backend/routes/userGrow.js
+++ app-backend/routes/userGrow.js
@@ -15,16 +15,14 @@ router.post('/getAllCropPlanted/:id', async (req, res) => {
 })
 
 //get list of crops planted by a user during a specific season
-router.post('/perSeason/:user/:season', async (req, res) => {
-    const {user, season} = req.params
+router.post('/perSeason/', async (req, res) => {
+    const {FK_userID, FK_seasonID} = req.body
     const results = await db('user-grow')
-    .where({FK_userID: user,
-    FK_seasonID: season})
-    console.log('results => ', results)
-    res.send(results)
+    .where({FK_userID, FK_seasonID})
+    res.status(300).send(results)
 })
 
-//save the data of a newly planted crop*/
+//save the data of a newly planted crop
 router.post('/saveUserGrow', async (req, res) => {
     const   {
                 userID, 
@@ -46,5 +44,20 @@ router.post('/saveUserGrow', async (req, res) => {
             FK_seasonID: seasonID
         }
     ).returning('*')
+    res.status(300).send(results)
+})
+
+//save changes to values of one crop planted
+router.put('/updateUserGrow', async (req, res) => {
+    const   {
+        userGrowID,
+        haveSpeedGro,
+        haveDeluxeSpeedGro,
+        dayOfSeason
+    } = req.body
+    const results = await db('user-grow')
+        .where({ userGrowID })
+        .update({ haveSpeedGro, haveDeluxeSpeedGro, dayOfSeason })
+        .returning('*')
     res.status(300).send(results)
 })
