cb94ccd0ccf23ca218b4f901b1454cfcbc70bfd6
rodrigo <rodri47hn@gmail.com>
2021-02-14 16:03:52 -0300

    finished howoto and abouproject page, missing style

--- app-frontend/src/App/components/downNavBar.js
+++ app-frontend/src/App/components/downNavBar.js
@@ -7,15 +7,6 @@ import {
 } from "react-router-dom";
 import './downNavBar.css'
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function BasicExample() {
   return (
     <Router>
@@ -32,13 +23,6 @@ export default function BasicExample() {
           </li>
         </ul>
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/how" render={ () => <How />}>
             <How />
@@ -55,13 +39,40 @@ export default function BasicExample() {
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
-
 function How() {
   return (
     <div>
       <h2>Hot to ...</h2>
+      <h3>...use this tool?</h3>
+      <p>This tool is to plan out any sesason by giving you a visual hint on when a crop would be ready to harvest based on the day planted.
+      For a basic use please follow the above instructions :</p>
+      <ul>
+        <li>Select the Season you are interested on planting, by default is Spring</li>
+        <li>Then inside the calendar select the appropiate Day to plant</li>
+        <li>Ones inside a Day, a window that show the current planted and harvestable crop would appear</li>
+        <li>On the top of the window a select box would appear with all the available crop for the season</li>
+        <li>Select one of the crops and press "Add" to be added on the list below</li>
+      </ul>
+      <h3>...save my calendar</h3>
+      <p>You are allow to save your progress to keep working later after doing any change. This information is stored locally, so if the browser data 
+      is deleted, your progress is too, so beware.</p>
+      <ul>
+        <li>Ones a change is made, an up-facing arrow would appear in the calendar's top-right corner</li>
+        <li>Press such arrow to save your calendar</li>
+        <li>After the save is done, the arrow would disappear until a new change happend to the calendar</li>
+      </ul>
+      <h3>...load my saved calendar</h3>
+      <p>By default the tool check if there is a saved calendar on the browser and if there is, it's automatically loaded and ready to work</p>
+      <ul>
+        <li>Ones a change is made, a down-facing arrow appear in the calendar's top-right corner</li>
+        <li>If at any point, you would like to discard changes and come back to the saved state, just press it.</li>
+      </ul>
+      <h3>...clear the screen</h3>
+      <p>Maybe you would like to start all over again, maybe something don't look right, whatever is the reason you can erase all the crops planted and start with a blank canvas.
+        Quick side note here, this does not modify the saved data, unless you save the blank calendar, and this clear is done just to the current season, leaving the other unaffected. </p>
+        <ul>
+          <li>To clear the current sesason's calendar, press the red button at the calendar's top-right corner</li>
+        </ul>
     </div>
   );
 }
@@ -70,6 +81,16 @@ function About() {
   return (
     <div>
       <h2>About the project</h2>
+      <p>This project was build as a way to showcase my ability as a fullstack developer. If by any chance you are interested on hire me as a Junior fullstack Developer, my <a href="https://www.linkedin.com/in/rodrigo-portugal/">Linkedin</a></p>
+      <h3>Credits:</h3>
+      <p></p>
+      <ul>
+        <li>Stardew Valley by <b>ConcernedApe</b>, your game is awesome and have given me lots of fun hours</li>
+        <li>Tools used: <b>Visual Studio Code</b>, <b>PgAdmin</b>, <b>Postman</b>, lots of tutorials and even more StackOverflow questions</li>
+        <li>All the crops and information needed were from the <b>Stardew Valley Wiki</b></li>
+        <li>For taking my almost daily spam about every little change and progress, and push me to have a working version and publish it, <a href="https://twitter.com/AgLeon4"><b>@AgLeon4</b></a></li>
+        <li>All the background image, UI elements and crops images were taken from the <a href="https://store.steampowered.com/app/413150/Stardew_Valley/"><b>Stardew Valley game by ConcernedApe</b></a></li>
+      </ul>
     </div>
   );
 }
