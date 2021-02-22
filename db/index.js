e83f60c0b249f65d5fc2e1668c5c59e19b47295e
rodrigo <rodri47hn@gmail.com>
2021-02-22 19:22:48 -0300

    problem with ssl auth

--- db/index.js
+++ db/index.js
@@ -9,7 +9,10 @@ const devConfig = {
 }
 
 const prodConfig = {
-    connectionString : process.env.DATABASE_URL
+    connectionString : process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 const knex = require('knex')({
